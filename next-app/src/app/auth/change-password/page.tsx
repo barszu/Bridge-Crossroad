e9d39bf148193cc595a0c486aac97ee539f338d2
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2025-05-24 16:38:29 +0200

    font config, svg added

---
+++ next-app/src/app/auth/change-password/page.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface IChangePasswordPageProps {}
+
+export default function ChangePasswordPage(props: IChangePasswordPageProps) {
+  return <></>;
+}
--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -2,11 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ChakraCustomProvider from "@/components/chakra-config/custom-provider";
 import ReactQuerryProvider from "@/components/external-libs/react-querry/provider";
+import { Montserrat } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Bridge CrossRoad",
   description: "Autorzy: Szymon Kubiczek, Bartłomiej Szubiak, Joanna Konieczny",
 };
+
+const montserrat = Montserrat({
+  subsets: ["latin", "latin-ext"],
+  variable: "--font-montserrat",
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning lang="pl">
+    <html suppressHydrationWarning lang="pl" className={montserrat.variable}>
       <body>
         <ChakraCustomProvider>
           <ReactQuerryProvider>{children}</ReactQuerryProvider>
